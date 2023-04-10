291e47ddb9fbb52311b0f924309593552358cc6f
Kato Wildiers <kato.wildiers@student.kuleuven.be>
2023-04-10 18:11:23 +0200

    house and square

--- Assets/Scripts/ringMover.cs
+++ Assets/Scripts/ringMover.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 
 public class ringMover : MonoBehaviour
 {
-    private IEnumerator Waiter()
+    public int level = 1;
+
+    private IEnumerator Square()
     {
         for (int i = 0; i < 100; i++)
         {
             transform.localPosition += new Vector3(0, (float)0.008, 0);
             yield return new WaitForSeconds((float)0.005);
         }
+        GameObject.Find("Ring").GetComponent<Renderer>().material.color = new Color(250, 0, 0, 100f);
         for (int i = 0; i < 100; i++)
         {
             transform.localPosition += new Vector3((float)0.008, 0, 0);
             yield return new WaitForSeconds((float)0.005);
         }
+        GameObject.Find("Ring").GetComponent<Renderer>().material.color = new Color(0, 250, 0, 100f);
         for (int i = 0; i < 100; i++)
         {
             transform.localPosition += new Vector3(0, (float)-0.008, 0);
@@ -27,35 +31,105 @@ public class ringMover : MonoBehaviour
             transform.localPosition += new Vector3((float)-0.008, 0, 0);
             yield return new WaitForSeconds((float)0.005);
         }
-    }
 
-    private IEnumerator Square()
-    {
-        Vector3 startPos = transform.localPosition;
-        Vector3 targetPos = startPos + new Vector3(0f,5f,0f);
-        float speed = 1f;
-        bool finished= false;
-        
-        /*
-        while (!finished)
+        level = 2;
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < 10; i++)
         {
-            transform.localPosition = Vector3.MoveTowards(startPos, targetPos, speed * Time.deltaTime);
-            if(startPos == targetPos)
-            {
-                finished = true;
-            }
-            // Hier dan een if statement die de target en startpos aanpast als ge aan een hoek bent. 
+            transform.localPosition += new Vector3((float)0.020, (float)0.010, 0);
+            yield return new WaitForSeconds((float)0.001);
         }
-        */
-        yield return new WaitForSeconds((float)0.005);       
+
     }
 
+
+    private IEnumerator House()
+    {
+        GameObject.Find("Ring").GetComponent<Renderer>().material.color = new Color(250, 0, 0, 60);
+
+        for (int i = 0; i < 50; i++)
+        {
+            transform.localPosition += new Vector3(0, (float)0.008, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+        for (int i = 0; i < 50; i++)
+        {
+            transform.localPosition += new Vector3((float)0.008, 0, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+        for (int i = 0; i < 50; i++)
+        {
+            transform.localPosition += new Vector3(0, (float)-0.008, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+        for (int i = 0; i < 50; i++)
+        {
+            transform.localPosition += new Vector3((float)-0.008, 0, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+        for (int i = 0; i < 50; i++)
+        {
+            transform.localPosition += new Vector3(0, (float)0.008, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+        for (int i = 0; i < 25; i++)
+        {
+            transform.localPosition += new Vector3((float)0.01, (float)0.008, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+        for (int i = 0; i < 25; i++)
+        {
+            transform.localPosition += new Vector3((float)0.01, (float)-0.008, 0);
+            yield return new WaitForSeconds((float)0.005);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Marker"))
         {
-            StartCoroutine(Square());
+            if (level == 1)
+            {
+                StartCoroutine(Square());
+            }
+            if (level == 2)
+            {
+                StartCoroutine(House());
+            }
+        }
+    }
+
+    private IEnumerator moveLeft(int amount, float speed) 
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            transform.localPosition += new Vector3((float)-0.008, 0, 0);
+            yield return new WaitForSeconds((float)speed);
+        }
+    }
+    private IEnumerator moveUp(int amount, float speed)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            transform.localPosition += new Vector3(0, (float)0.008, 0);
+            yield return new WaitForSeconds((float)speed);
+        }
+    }
+    private IEnumerator moveRight(int amount, float speed)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            transform.localPosition += new Vector3((float)0.008, 0, 0);
+            yield return new WaitForSeconds((float)speed);
+        }
+    }
+    private IEnumerator moveDown(int amount, float speed)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            transform.localPosition += new Vector3(0, (float)-0.008, 0);
+            yield return new WaitForSeconds((float)speed);
         }
     }
 }
