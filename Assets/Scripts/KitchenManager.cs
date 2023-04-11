6e98e0dacb90e07ab61e901d0c5f1aade40ff2f1
tinewildiers <62386767+tinewildiers@users.noreply.github.com>
2023-04-11 11:59:12 +0200

    test

---
+++ Assets/Scripts/KitchenManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenManager : MonoBehaviour
+{
+    public GameObject XROrigin;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
